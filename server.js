38f1845982a5125721c44e3c648e55c03720355d
Howard Mall <howardmall+gitkraken@howardmall.net>
2020-10-24 00:19:01 -0400

    Made it so it is HTTPS

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require('fs');
+var https = require('https')
 var bodyParser = require('body-parser');
 var path = require('path');
 var pimp = require('./routes/pimp');
@@ -9,15 +11,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
- 
 app.get('/pimp', pimp.findAll);
 app.get('/pimp/:id', pimp.findById);
 app.post('/pimp', pimp.addItem);
 app.put('/pimp/:id', pimp.updateItem);
 app.delete('/pimp/:id', pimp.deleteItem);
 
-var port = 80;
-console.log('Listening on port '+port+'...');
-app.listen(port);
+var port = 443;
+
+https.createServer({
+	key: fs.readFileSync('pimp.key'),
+	cert: fs.readFileSync('pimp.cert')
+}, app)
+.listen(port, function () {
+	console.log('Listening on port '+port+'...');
+});
